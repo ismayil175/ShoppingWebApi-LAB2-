b903ed790fd91a64e072a76e95842919462188ad
ISMAYIL MAHARRAMOV <114576055+ismayil175@users.noreply.github.com>
2023-01-04 19:34:41 +0200

    Update Program.cs

--- ShoppingWebApi/Program.cs
+++ ShoppingWebApi/Program.cs
@@ -8,8 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<EF_DataContext>(
                 o => o.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL 15"))
-            );
-}
+  };
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

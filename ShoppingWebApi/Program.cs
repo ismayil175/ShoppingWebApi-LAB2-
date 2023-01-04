b2910ea4dcedcfa54500f53145175c7ecd6e6106
ISMAYIL MAHARRAMOV <114576055+ismayil175@users.noreply.github.com>
2023-01-04 18:21:55 +0200

    Update Program.cs

--- ShoppingWebApi/Program.cs
+++ ShoppingWebApi/Program.cs
@@ -6,22 +6,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 
+builder.Services.AddDbContext<EF_DataContext>(
+                o => o.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL 15"))
+            );
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ProductContext>(options =>
-    options
-        .UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL 15")
-        .UseSnakeCaseNamingConvention());
-
-builder.Services.AddScoped<ProductService>();
-
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+   
+}
 
 app.UseHttpsRedirection();
 
